28df3331ba64f0950dd40903c8f92c023149de16
xiezhizheng <870944165@qq.com>
2021-06-07 17:40:58 +0800

    配置

--- rabbitMq/src/main/java/Config.java
+++ rabbitMq/src/main/java/Config.java
@@ -1,2 +1,10 @@
+import com.rabbitmq.client.impl.AMQImpl;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Config {
+
+    public AMQImpl.Queue commonQueue(){
+        return null;
+    }
 }
--- rabbitMq/src/main/java/Controller.java
+++ rabbitMq/src/main/java/Controller.java
@@ -1,2 +1,6 @@
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class Controller {
+
 }
